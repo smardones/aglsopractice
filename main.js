9ba93dea095c48b36e2094ffb512d79f28f1c36f
smardone <shaun.mardones@gmail.com>
2021-05-19 21:07:47 -0400

    grading students

--- main.js
+++ main.js
@@ -2,3 +2,22 @@ function timeConversion(s) {
     // Write your code here
 
 }
+
+// Grading Students Hackerrank
+
+function gradingStudents(grades) {
+    let answerArr = [];
+    let nextLevelArr = grades.map(grade => Math.ceil(grade / 5) * 5)
+    
+   for (let i = 0; i < grades.length; i++) {
+       if (grades[i] < 38) {
+           answerArr.push(grades[i]);
+       } else if ((nextLevelArr[i] - grades[i]) < 3) {
+           answerArr.push(nextLevelArr[i]);
+       } else if (!(nextLevelArr[i] - grades[i] < 3)) {
+           answerArr.push(grades[i]);
+       }
+   }
+   
+   return answerArr;
+}
