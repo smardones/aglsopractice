19001c6cb1b731bbd0be4bc51a335db69c85e8bc
smardone <shaun.mardones@gmail.com>
2021-08-22 20:24:18 -0400

    divisible sum

--- main.js
+++ main.js
@@ -117,3 +117,19 @@ function miniMaxSum(arr) {
     
     console.log(min + " " + max);
 }
+
+// Divisble Sum - Hackerrank
+
+function divisibleSumPairs(n, k, ar) {
+    let pairs = 0;
+    
+    
+    for (let i = 0; i < ar.length; i++) {
+        for (let j = 0; j < ar.length; j++) {
+            if ((ar[i] + ar[j]) % k === 0 && i < j) {
+                pairs++;
+            } 
+        }
+    }
+    return pairs;
+}
