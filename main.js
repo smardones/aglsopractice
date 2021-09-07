6145eaa1cfd7019b308fd97f634853e8fc95c36f
smardone <shaun.mardones@gmail.com>
2021-09-06 20:02:53 -0400

    Find closest value in BST

--- main.js
+++ main.js
@@ -220,3 +220,28 @@ function sockMerchant(n, ar) {
     return pairCount;
     
 }
+
+// Find Closest Value in Binary Search Tree - AlgoExpert
+
+function findClosestValueInBst(tree, target) {
+	return findClosestHelper(tree, target, tree.value);
+}
+
+
+
+function findClosestHelper(tree, target, closest) {
+	if (tree == null) {
+		return closest;
+	} 
+	if (Math.abs(target - closest) > Math.abs(target - tree.value)) {
+		closest = tree.value;
+	}
+	if (target > tree.value) {
+		return findClosestHelper(tree.right, target, closest);
+	} else if (target < tree.value) {
+		return findClosestHelper(tree.left, target, closest);
+	} else {
+		return closest;
+	}
+	
+}
