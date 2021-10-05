b04c52e08ad71da0486fb9f22f334b124aacb8ac
smardone <shaun.mardones@gmail.com>
2021-10-04 20:37:36 -0400

    binary tree depth

--- main.js
+++ main.js
@@ -640,4 +640,10 @@ return this;
         
         return answerArr;
     }
+
+    function nodeDepths(root, depth=0) {
+	
+        if (root === null) return 0;
+        return depth += nodeDepths(root.left, depth + 1) + nodeDepths(root.right, depth + 1);
+    }
       
